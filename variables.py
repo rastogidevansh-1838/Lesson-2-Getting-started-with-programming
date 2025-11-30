0adb9909875d680b9cd7b20a56df831ff841eed0
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-11-30 10:37:22 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 5
+y = "Devansh"
+print (x)
+print (y)
+codingal = "codingal"
+print (codingal)
+#user input
+name = input ("Enter your name: ")
+print("\n Hello", name, "\nwelcome to codingal")
