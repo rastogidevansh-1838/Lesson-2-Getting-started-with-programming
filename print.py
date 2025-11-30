c971a435a56fddc824b51aceaa7d222cd94a8e47
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-11-30 10:25:32 +0530

    print

---
+++ print.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print("Welcome to the world of Programming")
+#Print a number
+print(4)
+#Next line after print
+print("hello there! \n")
+#Pass multiple arguments for printing
+print("hello",5)
+#End argument of print statement
+print("Welcome to" , end= " woah!!")
