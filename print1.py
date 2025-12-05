a9d7e472afe896843171f51d86c4def542120bf1
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-05 18:16:52 +0530

    Add files via upload

---
+++ print1.py
@@ -0,0 +1,8 @@
+#Print a Sentence
+print("The Birthday Data!")
+#Next line after print
+print("Devansh Rastogi:- 18/05/2014")
+#Print a Sentence with numbers
+print("Kopal Rastogi:- 06/10/1985")
+#Next Line after print
+print("Mohit Kumar:- 24/12/1983")
